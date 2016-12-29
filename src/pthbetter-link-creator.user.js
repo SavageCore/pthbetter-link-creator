e816e109a530ff51e6d7e57e647f9c34bb94e08a
SavageCore <talk@savagecore.eu>
2016-12-29 20:19:21 +0000

    0.1.16

--- src/pthbetter-link-creator.user.js
+++ src/pthbetter-link-creator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PTH pthbetter Link Creator
 // @namespace    http://savagecore.eu/
-// @version      0.1.15
+// @version      0.1.16
 // @description  Generate pthbetter command and copy to clipboard
 // @author       SavageCore
 
